d18b1dc83089569ab5571742fddb1dd79b42df46
Pramod Kumar <pro1625@gmail.com>
2023-05-30 15:51:17 +0530

    routes added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,5 @@
-// import React from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-
-// function Navbar() {
-//   return (
-//     <div>
-//         <Link to='/' >Home</Link>
-//         <Link to='/login' >Login</Link>
-//         <Link to='/signup' >Signup</Link>
-//     </div>
-//   )
-// }
-
-// export default Navbar
-
-import React from 'react'
 import styles from './Navbar.module.css'
 
 const Navbar = () => {
