22e439d03cff10d5516267520a226825d2b0bb01
RohitMBhosale <bhosalerohit204@gmail.com>
2023-06-01 07:51:04 +0530

    Login Validation and styling changes done

--- src/Component/Login/login.js
+++ src/Component/Login/login.js
@@ -15,65 +15,35 @@ import { IconButton } from '@mui/material';
 
 
 export default function Login() {
-  const setUser = useSetRecoilState(isUserSignedInAtom);
-  const userNameRef = useRef();
-  const passwordRef = useRef();
-  const [userList, setUserList] = useState([
-    {
-      username: "",
-      password: "",
-    },
-  ]);
-  const nav = useNavigate();
 
-  useEffect(() => {
-    const data = getUsers();
-    setUserList(data);
-  }, []);
 
-  // Check if user is already signed in, and redirect to home page
-  useEffect(() => {
-    const isSignedIn = localStorage.getItem("isSignedIn");
-    if (isSignedIn === "true") {
-      setUser(true);
-      nav("/");
-    }
-  }, [setUser, nav]);
+  let userArr=JSON.parse(localStorage.getItem("users"));
+  // console.log(userArr)
+ const navigate =  useNavigate();
+function handleSubmit(event){
+      event.preventDefault();
+     userArr.map(function(elem){
 
-  // const {
-  //   register,
-  //   handleSubmit,
-  //   formState: { errors },
-  // } = useForm();
-  // const onSubmit = (e) => {
-  //   alert("data submitted");
-  // };
+        let userEmail = document.getElementById("email").value;
+        let userPass = document.getElementById("pass").value;
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    if (userNameRef.current.value === "" || passwordRef.current.value === "") {
-      alert("Fill the form first");
-    }
+        console.log(userEmail, userPass, elem.email , elem.password );
+        
 
-    const userObj = userList.find(
-      (obj) =>
-        obj?.username === userNameRef.current.value &&
-        obj?.password === passwordRef.current.value
-    );
+        if( elem.email == userEmail && elem.password == userPass)
+         {
+                alert("Login Successfull");
+                navigate('/')
+               
+         }
+            else if(elem.pass !== userPass || elem.email !== userEmail )
+            {
+                alert("Invalid Email OR Passward");
+            }
 
-    if (userObj !== undefined) {
-      localStorage.setItem("isSignedIn", "true");
-      setUser(true);
-      alert(`${userObj?.username} you are successfully logged in`);
-      nav("/");
-    } else {
-      alert("Please register first");
-    }
-  }
+    })
 
-    function handleSignUp(){
-    nav('/signup')
-  }
+}
 
   return (
     <>
@@ -85,13 +55,13 @@ export default function Login() {
 
         <h1 style={{ marginBottom: "15px" }}>Sign in to Twitter</h1>
         <button className={styles.btn}>
-          <FcGoogle size={20} style={{ marginRight: "5px" }} />
+          <FcGoogle size={19} style={{ marginRight: "5px" }} />
           Sign up with Google
         </button>
         
 
         <button className={styles.btn}>
-          <FaApple size={20} style={{ marginRight: "5px" }} />
+          <FaApple size={19} style={{ marginRight: "5px" }} />
           Sign up with Apple
         </button>
         <hr></hr>
@@ -100,27 +70,30 @@ export default function Login() {
         <form className={styles.form} onSubmit={handleSubmit}>
           <input
             className={styles.input}
-            ref={userNameRef}
-            type="username"
-            placeholder="username"
+            // ref={userNameRef}
+            type="email"
+            id="email"
+            placeholder="Email"
             required
           /><br/>
           <input
             className={styles.input}
-            ref={passwordRef}
+            // ref={passwordRef}
             type="password"
-            placeholder="password"
+            id="pass"
+            placeholder="Password"
             required
           />
           <br />
-          <Button
+          {/* <Button
             className={styles.login_btn}
             variant="contained"
             type="submit"
             sx={{ borderRadius: 10, backgroundColor: "black", color: "White" }}
           >
             Login
-          </Button>
+          </Button> */}
+          <input  className={styles.input_submit} type="submit" id="login"/>
         </form>
 
         <button
